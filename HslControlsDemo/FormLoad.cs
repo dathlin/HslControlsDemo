50672da294c199fe94924b1fd5e84155c4d7a7a5
dathlin <hsl200909@163.com>
2019-01-31 10:27:30 +0800

    feat(release): v1.2.3

--- HslControlsDemo/FormLoad.cs
+++ HslControlsDemo/FormLoad.cs
@@ -174,7 +174,7 @@ namespace HslControlsDemo
             System.Threading.ThreadPool.QueueUserWorkItem( new System.Threading.WaitCallback( ThreadPoolCheckVersion ), null );
         }
 
-        private HslCommunication.BasicFramework.SystemVersion versionCurr = new HslCommunication.BasicFramework.SystemVersion( "1.2.2" );
+        private HslCommunication.BasicFramework.SystemVersion versionCurr = new HslCommunication.BasicFramework.SystemVersion( "1.2.3" );
 
         private void ThreadPoolCheckVersion( object obj )
         {
